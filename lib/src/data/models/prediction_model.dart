251ba8cd4af75042cf9315ed963c67af67652b91
Therry Martins <therry.martins@outlook.com>
2024-01-08 08:37:02 +0200

    updated PredictionModel data model

--- lib/src/data/models/prediction_model.dart
+++ lib/src/data/models/prediction_model.dart
@@ -44,16 +44,46 @@ class PredictionModel extends PredictionEntity {
             : List<String>.from(json["types"]!.map((x) => x)),
       );
 
+
   Map<String, dynamic> toMap() {
-    return <String, dynamic>{
-      'description': description,
-      'matchedSubstrings': matchedSubstrings,
-      'placeId': placeId,
-      'reference': reference,
-      'structuredFormatting': structuredFormatting,
-      'terms': terms,
-      'types': types
+    return {
+      'latitude': this.latitude,
+      'longitude': this.longitude,
+      'description': this.description,
+      'matchedSubstrings': this.matchedSubstrings,
+      'placeId': this.placeId,
+      'reference': this.reference,
+      'structuredFormatting': this.structuredFormatting,
+      'terms': this.terms,
+      'types': this.types,
     };
   }
+  @override
+  String toString() {
+    return 'PredictionModel{' +
+        ' latitude: $latitude,' +
+        ' longitude: $longitude,' +
+        ' description: $description,' +
+        ' matchedSubstrings: $matchedSubstrings,' +
+        ' placeId: $placeId,' +
+        ' reference: $reference,' +
+        ' structuredFormatting: $structuredFormatting,' +
+        ' terms: $terms,' +
+        ' types: $types,' +
+        '}';
+  }
 
+  factory PredictionModel.fromMap(Map<String, dynamic> map) {
+    return PredictionModel(
+      latitude: map['latitude'] as double,
+      longitude: map['longitude'] as double,
+      description: map['description'] as String,
+      matchedSubstrings: map['matchedSubstrings'] as List<MatchedSubstring>,
+      placeId: map['placeId'] as String,
+      reference: map['reference'] as String,
+      structuredFormatting: map['structuredFormatting'] as StructuredFormatting,
+      terms: map['terms'] as List<Term>,
+      types: map['types'] as List<String>,
+    );
+  }
 }
